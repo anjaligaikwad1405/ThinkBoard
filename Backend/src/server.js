9329097f289cbfa5bcc516b1b9caaf0a5f3ff580
anjaligaikwad1405 <147701290+anjaligaikwad1405@users.noreply.github.com>
2025-06-19 01:34:23 +0530

    next commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,11 +5,19 @@ import dotenv from "dotenv"
 import rateLimiter from "./middleware/ratelimiter.js"
 import cors from "cors"
 dotenv.config();
+const __dirname = path.resolve();
 
 const app=express();
 const port=process.env.PORT||5001;
 
 //middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 app.use(
@@ -19,6 +27,15 @@ app.use(
     }
 ));
 app.use("/api/notes",notesRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/vite/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDb().then(()=>{
     app.listen(5001, ()=>{
     console.log("server started on port ",port);
